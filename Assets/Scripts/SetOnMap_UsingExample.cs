0bc5da039311cdef231960e7f9a2b4b89e5905d0
denis <arvin.esagholian@stundets.hs-mainz.de>
2023-06-06 22:58:33 +0200

    Pointer mit richtige Koordinaten hinzugefügt

--- Assets/Scripts/SetOnMap_UsingExample.cs
+++ Assets/Scripts/SetOnMap_UsingExample.cs
@@ -7,6 +7,7 @@ public class SetOnMap_UsingExample : MonoBehaviour
 
     SetOnMap geoScript;
 
+    public GameObject originalObject;
 
     private List<(GameObject, Vector3)> exampleList;
 
@@ -15,17 +16,19 @@ public class SetOnMap_UsingExample : MonoBehaviour
 
     void Start()
     {
+        GameObject newObject = Instantiate(originalObject, originalObject.transform.position, originalObject.transform.rotation);
+        
         geoScript = gameObject.GetComponent<SetOnMap>();
 
         // obj
         GameObject exampleObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        exampleObj.transform.localScale = new Vector3(500000, 500000, 500000);
+        exampleObj.transform.localScale = new Vector3(200000, 200000, 200000);
         
         exampleObj.GetComponent<Renderer>().material.color = Color.red;
 
         // add values
         exampleList = new List<(GameObject, Vector3)>();
-        exampleList.Add((exampleObj, new Vector3(-17.783517f, 31.053044f, 43112.24f)));
+        exampleList.Add((newObject, new Vector3(-17.783517f, 31.053044f, 0)));
         exampleList.Add((exampleObj, new Vector3(49.98948486726362f, 8.229072290561168f, 180f)));
         exampleList.Add((exampleObj, new Vector3(50.00136027689663f, 8.25901393590933f, 180f)));
 
