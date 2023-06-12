f28c8d6c34d74a52f50badaabb2a571eb8d9f9a6
denis <arvin.esagholian@stundets.hs-mainz.de>
2023-06-12 23:24:34 +0200

    Map geändert, paar fixes und PoinInfo Funktion ergänzt

---
+++ Assets/Scripts/CloseUI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseUI : MonoBehaviour
+{
+    public void closeUI(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+}
