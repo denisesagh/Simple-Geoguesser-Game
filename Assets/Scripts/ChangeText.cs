b5dcdffd3e380ab230e610f1482059d0deb26a14
jonaskornau <jonas.kornau@students.hs-mainz.de>
2023-06-15 18:15:46 +0200

    enhanced Placesinformation

--- Assets/Scripts/ChangeText.cs
+++ Assets/Scripts/ChangeText.cs
@@ -4,6 +4,8 @@ using System.Net.Mime;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using UnityEngine.Serialization;
 
 public class ChangeText : MonoBehaviour
 {   
@@ -14,6 +16,43 @@ public class ChangeText : MonoBehaviour
     public TextMeshProUGUI partner;
 
     public TextMeshProUGUI gefordertDurch;
+
+    public TextMeshProUGUI distanzNachMainz;
+
+    public Button openLinkButton;
+    
+    public void OpenLink()
+    {
+        Application.OpenURL(url);
+    }
+    public static double CalculateDistance(double lat1, double lon1, double lat2, double lon2)
+    {
+        const double radius = 6371; // Earth's radius in kilometers
+
+        double lat1Rad = ToRadians(lat1);
+        double lon1Rad = ToRadians(lon1);
+        double lat2Rad = ToRadians(lat2);
+        double lon2Rad = ToRadians(lon2);
+
+        double deltaLat = lat2Rad - lat1Rad;
+        double deltaLon = lon2Rad - lon1Rad;
+
+        double a = Math.Sin(deltaLat / 2) * Math.Sin(deltaLat / 2) +
+                   Math.Cos(lat1Rad) * Math.Cos(lat2Rad) *
+                   Math.Sin(deltaLon / 2) * Math.Sin(deltaLon / 2);
+
+        double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+        double distance = radius * c;
+        distance = Math.Round(distance, 2);;
+        return distance;
+    }
+    public string url;
+
+    private static double ToRadians(double degrees)
+    {
+        return degrees * Math.PI / 180;
+    }
     
     
     string[,] zimbabweData = {
@@ -39,17 +78,16 @@ public class ChangeText : MonoBehaviour
     string[,] qatnaData = {
         {"Qatna (Syrien)", "34.8892782", "36.5172377", "Mediale Verdeutlichung der Ergebnisse und Tätigkeiten im Kontext der archäologischen Ausgrabung zu Qatna", "Universität Tübingen", "DFG", "https://i3mainz.hs-mainz.de/projekte/mediale-verdeutlichung-qatna/"}
     };
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        string location = PlayerPrefs.GetString("location");
+        string location = PlayerPrefs.GetString("infoPointer");
         if (location == null)
         {
             location = "Zimbabwe";
         }
         string[,] data = new string[,] { };
-        print(location);
         switch (location)
         {
             case "Zimbabwe":
@@ -91,9 +129,22 @@ public class ChangeText : MonoBehaviour
         
         ort.text = data[0,0];
         title.text = data[0, 3];
-        partner.text = data[0, 4];
-        gefordertDurch.text = data[0, 5];
+        partner.text = "Partner:\n"+ data[0, 4];
+        gefordertDurch.text ="Gefördert Durch " +data[0, 5];
+        print(double.Parse(data[0, 2]));
+       // print(CalculateDistance(49.9929, 8.2473, double.Parse(data[0, 2]), double.Parse(data[0, 3])));
+       String dis= String.Format("{0}",CalculateDistance(49.9929, 8.2473, double.Parse(data[0, 1] ),double.Parse(data[0, 2] )));
+       
+       distanzNachMainz.text = dis+" Km entfernung nach Mainz";
+       url = data[0,6];
+       openLinkButton.onClick.AddListener(OpenLink);
+       openLinkButton.GetComponentInChildren<TextMeshProUGUI>().text = data[0, 6];
     }
+
+    
+
+    
+    
 
     // Update is called once per frame
 
